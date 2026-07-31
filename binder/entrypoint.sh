51b8f0e11f6aaca7ac379539f96c98789dcc3af9
yxzhan <zhanyanxiang@hotmail.ccom>
2026-07-31 11:35:57 +0000

    delete tmp testing files

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,7 +3,7 @@
 # Launcher page: static http server, reachable via jupyter-server-proxy at
 # /user/<name>/proxy/8899/. Wrapped in a loop so it is respawned if it ever dies.
 nohup bash -c 'while true; do
-    python3 -m http.server 8899 --bind 127.0.0.1 --directory $PWD/demo/web_ui
+    python3 -m http.server 8899 --bind 127.0.0.1 --directory $PWD/demos/web_ui
     echo "[ebim-launcher] http.server exited, restarting in 2s..."
     sleep 2
 done' >/tmp/ebim-launcher.log 2>&1 &
