59bf2bb077ee67e9448c3a5a4e4dffbbfd7505c3
yxzhan <zhanyanxiang@hotmail.ccom>
2026-08-25 13:18:11 +0000

    update entrypoint

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Launcher page: static http server, reachable via jupyter-server-proxy at
-# /user/<name>/proxy/8899/. Wrapped in a loop so it is respawned if it ever dies.
-nohup bash -c 'while true; do
-    python3 -m http.server 8899 --bind 127.0.0.1 --directory $PWD/demos/web_ui
-    echo "[ebim-launcher] http.server exited, restarting in 2s..."
-    sleep 2
-done' >/tmp/ebim-launcher.log 2>&1 &
-
 # The following line will allow the binderhub start Jupyterlab, should be at the end of the entrypoint.
 exec "$@"
